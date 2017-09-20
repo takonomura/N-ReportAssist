6f59e2c72fdf84ede080213f89810fe8f572caed
takonomura <takonomura@users.noreply.github.com>
2017-09-20 14:35:04 +0900

    Add deadlines for all subjects

--- report-list.js
+++ report-list.js
@@ -12,22 +12,50 @@ const DEADLINE_NAMES = ['5月', '6月', '7月', '8月', '9月', '10月', '11月'
 function getReportDeadline(subjectName, reportIndex) {
   const PATTERN_12 = [0, 1, 1, 2, 2, 3, 4, 4, 5, 6, 6, 7];
   const PATTERN_9 = [0, 1, 2, 3, 4, 5, 6, 6, 7];
+  const PATTERN_8 = [0, 1, 2, 3, 4, 5, 6, 7];
   const PATTERN_6A = [0, 1, 2, 5, 6, 7];
   const PATTERN_6B = [1, 2, 3, 5, 6, 7];
   const PATTERN_4 = [1, 3, 5, 7];
+  const PATTERN_3 = [2, 5, 7]
   const PATTERN_2 = [2, 7];
   const PATTERN_1 = [7];
   const DEADLINE = {
     '国語総合': PATTERN_12,
+    '国語表現': PATTERN_9,
+    '現代文A': PATTERN_6A,
+    '現代文B': PATTERN_12,
+    '古典A': PATTERN_6A,
+    '世界史B': PATTERN_12,
     '日本史B': PATTERN_12,
+    '地理B': PATTERN_12,
     '現代社会': PATTERN_6A,
+    '倫理': PATTERN_6A,
+    '政治・経済': PATTERN_6A,
     '数学I': PATTERN_9,
+    '数学II': PATTERN_12,
+    '数学A': PATTERN_6A,
+    '数学B': PATTERN_6A,
     '科学と人間生活': PATTERN_6A,
+    '物理基礎': PATTERN_6A,
+    '化学基礎': PATTERN_6A,
+    '生物基礎': PATTERN_6A,
+    '地学基礎': PATTERN_6A,
     '体育I': PATTERN_2,
+    '体育II': PATTERN_3,
+    '体育III': PATTERN_2,
+    '保健': PATTERN_6B,
     '美術I': PATTERN_6B,
     'コミュニケーション英語I': PATTERN_9,
+    'コミュニケーション英語II': PATTERN_12,
+    '英語表現I': PATTERN_6A,
+    '英語会話': PATTERN_6A,
     '家庭基礎': PATTERN_4,
+    '家庭総合': PATTERN_8,
+    '社会と情報': PATTERN_4,
+    '情報の科学': PATTERN_4,
     '総合的な学習I': PATTERN_1,
+    '総合的な学習II': PATTERN_1,
+    '総合的な学習III': PATTERN_1,
   };
   if (!DEADLINE[subjectName]) {
     console.warn('Unknown subject name:', subjectName);
